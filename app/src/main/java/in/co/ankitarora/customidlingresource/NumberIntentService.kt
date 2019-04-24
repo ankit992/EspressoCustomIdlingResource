447d89f7478b2ad366edcd61ab849e98ab11b942
ankitarora <ankit992@live.in>
2019-04-24 08:52:14 +0530

    Adding number intent service with an intentional wait of 10 seconds, to replicate a long-running background task

---
+++ app/src/main/java/in/co/ankitarora/customidlingresource/NumberIntentService.kt
@@ -0,0 +1,17 @@
+package `in`.co.ankitarora.customidlingresource
+
+import android.app.IntentService
+import android.content.Intent
+import java.lang.Thread.sleep
+
+class NumberIntentService : IntentService("NumberIntentService") {
+    override fun onHandleIntent(p0: Intent?) {
+        val input = p0?.getIntExtra("Hello", 0)
+        sleep(10000)
+        val outputValue = input?.plus(1)
+        val outputIntent = Intent("output")
+        outputIntent.putExtra("output", input)
+        sendBroadcast(outputIntent)
+    }
+
+}
